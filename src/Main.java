41c371c5a04da02ee570cac1ff299a91bfe29ba0
WillOli <william.ika@gmail.com>
2023-08-25 08:33:19 -0300

    Fazendo ligacoes para cada candidato selecionado

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,41 @@
 import java.lang.reflect.AnnotatedArrayType;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
     public static void main(String[] args) {
-        imprimirSelecionador();
+        String [] candidatos =  {"FELIPE", "JULIA", "WILLIAM", "ALEXIA", "JULIA", "ROBERTO", "SILVA", "MARIA", "ROBERTA"};
+        for (String candidato: candidatos) {
+            entrandoEmContato(candidato);
+        }
 
+    }
+    static void entrandoEmContato(String candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+
+        do {
+            atendeu=atender();
+            continuarTentando = !atendeu;
+            if (continuarTentando){
+                tentativasRealizadas++;
+            }else
+                System.out.println("CONTATO REALIZADO COM SUCESSO!");
+
+        }while(continuarTentando && tentativasRealizadas< 3);
+
+        if(atendeu) {
+            System.out.println("CONSEGUIMOS CONTATO COM: " + candidato + "NA " + tentativasRealizadas + " TENTATIVA");
+        }else
+            System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato  + ", NÚMERO MÁXIMO TENTATIVAS " + tentativasRealizadas);
+    }
+
+
+
+    //método auxiliar
+    static boolean atender() {
+        return new Random().nextInt(3)==1;
     }
 
     static void imprimirSelecionador() {
@@ -22,8 +53,6 @@ public class Main {
 //            System.out.println("O candidato selecionado foi " + candidato);
 //        }
     }
-
-
     static void selecaoCandidatos() {
         String [] candidatos = {"FELIPE", "JULIA", "WILLIAM", "ALEXIA", "JULIA", "ROBERTO", "SILVA", "MARIA", "ROBERTA"};
         int candidatosSelecionados = 0;
