df9496dfdec4666460f46b10c3d479bc3ffe6a94
WillOli <william.ika@gmail.com>
2023-08-24 21:23:44 -0300

    Selecionando Candidatos

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,32 @@
 import java.lang.reflect.AnnotatedArrayType;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
     public static void main(String[] args) {
-        analisarCandidato(1900.0);
-        analisarCandidato(2000.0);
-        analisarCandidato(2200.0);
+        selecaoCandidatos();
 
+    }
+
+    static void selecaoCandidatos() {
+        String [] candidatos = {"FELIPE", "JULIA", "WILLIAM", "ALEXIA", "JULIA", "ROBERTO", "SILVA", "MARIA", "ROBERTA"};
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase = 2000.0;
+        while(candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " Solicitou este valor de salário " + salarioPretendido);
+            if(salarioBase >= salarioPretendido) {
+                System.out.println("O cadidato " + candidato + " foi selecionado para a vagar.");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
+        }
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
     }
 
     static void analisarCandidato(double salarioPretendido) {
